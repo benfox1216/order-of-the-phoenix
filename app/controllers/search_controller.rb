86bd74ed6beb901aff40bc69e50ebf2792816de7
Ben Fox <benfox1216@gmail.com>
2020-05-19 10:26:15 -0600

    Get successful response from HP api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,10 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new('https://www.potterapi.com/v1')
-    # response = conn.get("/houses?key=#{ENV['HP_API_KEY']}")
-    response = conn.get('/sortinghat')
+    conn = Faraday.new(url: 'https://www.potterapi.com/v1') do |f|
+      f.params['key'] = ENV['HP_API_KEY']
+    end
+    
+    response = conn.get("houses")
     json = JSON.parse(response.body, symbolize_names: true)
     binding.pry
   end
